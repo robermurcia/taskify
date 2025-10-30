8804f5e2ade170837fcb00185a76ab1ecd62ca6c
robermurcia <robertoomurciaa@gmail.com>
2025-10-30 01:21:29 +0100

    feat(api): create Spring Boot base project with MongoDB and Security dependencies

---
+++ backend/taskify-api/src/test/java/com/taskify/TaskifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
